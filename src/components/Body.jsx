eb1ea2a786fa78e9a854b8ea97134fbbcdcec613
unknown <sai.h152015@gmail.com>
2023-10-31 12:11:07 -0400

    Added Loading Button

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,11 +8,14 @@ function Body() {
     // const [errors, setErrors] = useState([])
     const [data, setData] = useState(null)
     const [status, setStatus] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [isLoadingResponse, setIsLoadingResponse] = useState(false)
     const [response, setResponse] = useState('yes')
     const [plusOneResponse, setPlusOneResponse] = useState('yes')
     
 
     const search = async () => {
+        setIsLoading(true)
         // setErrors([])
         if(!firstName || !lastName){
             alert('Please enter a first and last name')
@@ -25,18 +28,20 @@ function Body() {
             console.log(res)
             // console.log(res.data)
             // console.log(res.data.plusOne)
-            if(res.data){
+            if(res?.data){
                 setData(res.data)
                 setStatus(res.status)
             }
         }).catch(error => {
             console.log(error)
-            setData(error.response.data)
-            setStatus(error.response.status)
+            setData(error?.response?.data)
+            setStatus(error?.response?.status)
         })
+        setIsLoading(false)
     }
 
     const respond = async () => {
+        setIsLoadingResponse(true)
         // setErrors([])
         const url = `https://ss-rsvp-api.onrender.com/search/${response}?firstName=${firstName}&lastName=${lastName}`
         axios.patch(url)
@@ -45,14 +50,18 @@ function Body() {
                 console.log(res.data)
                 setData(res.data)
                 setStatus(res.status)
+                setIsLoadingResponse(false)
+                alert('Thank you for responding!')
+                window.location.reload();
             }
         }).catch(error => {
             console.log(error)
             setData(error.response.data)
             setStatus(error.response.status)
+            setIsLoadingResponse(false)
+            alert('Search Failed! Please Try Again!')
         })
-        alert('Thank you for responding!')
-        window.location.reload();
+        
         // setData(null)
         // setStatus(null)
         // setFirstName('')
@@ -74,7 +83,7 @@ function Body() {
             <input className="w-48 py-1 px-4 rounded-lg border-2 border-white bg-transparent placeholder:text-white" type="text" name="firstName" placeholder="First Name" value={firstName} onChange={handleChange}/>
             <input className="w-48 py-1 px-4 rounded-lg border-2 border-white bg-transparent placeholder:text-white" type="text" name="lastName" placeholder="Last Name" value={lastName} onChange={handleChange}/>
         </div>
-        <button className="w-32 py-1 px-4 rounded-lg bg-white border-2 border-[#5D2A42] text-[#FB6376]" onClick={search} >Search</button>
+        <button className="w-32 py-1 px-4 rounded-lg bg-white border-2 border-[#5D2A42] text-[#FB6376] disabled:bg-gray-300" disabled={isLoading} onClick={search} >{!isLoading ? 'Search' : 'Loading'}</button>
         {/* Unconfirmed Respdoned */}
         {data && data.response === 'Pending' && status === 200 && 
             <div>
@@ -105,7 +114,7 @@ function Body() {
                         </div>
                     </>
                 }
-                <button className="w-32 py-1 px-4 rounded-lg bg-white border-2 border-[#5D2A42] text-[#FB6376]" onClick={respond}>Respond</button>
+                <button className="w-32 py-1 px-4 rounded-lg bg-white border-2 border-[#5D2A42] text-[#FB6376]" disabled={isLoadingResponse} onClick={respond}>{!isLoadingResponse ? 'Respond' : 'Loading'}</button>
             </div>
         }
 
@@ -139,7 +148,7 @@ function Body() {
                         </div>
                     </>
                 }
-                <button className="mt-4 w-32 py-1 px-4 rounded-lg bg-white border-2 border-[#5D2A42] text-[#FB6376]" onClick={respond}>Respond</button>
+                <button className="mt-4 w-32 py-1 px-4 rounded-lg bg-white border-2 border-[#5D2A42] text-[#FB6376]" disabled={isLoadingResponse} onClick={respond}>{!isLoadingResponse ? 'Respond' : 'Loading'}</button>
             </div>
         }
 
@@ -147,6 +156,13 @@ function Body() {
         {!data && status === 404 && 
             <div>
                 <p>{firstName} {lastName} was not found.</p>
+            </div>
+        }
+
+        {/* Error */}
+        {!data && status !== null && status !== 404 && 
+            <div>
+                <p>Search Failed! Please Try Again!</p>
             </div>
         }
     </div>
